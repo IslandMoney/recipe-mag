edbcbc0e44fa592aa74523221157a0da487925c8
islandmoney <olgermile@yahoo.com>
2019-01-12 01:10:51 +0100

    Big Update: Added Routing App-Wide

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -16,6 +16,14 @@ export class RecipeService {
         [
             new Ingredient('Nuts', 5),
             new Ingredient('Kanell', 6)
+        ]),
+        new Recipe(
+            'Stuff',
+            'Very Tasty',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdWPLdnaI9sYx57_iK5Xu3oXmv_H7yQLn4uKMlxaPEYWpIwSjliw',
+        [
+            new Ingredient('Nuts', 5),
+            new Ingredient('Kanell', 6)
         ])
     ];
 
@@ -23,6 +31,10 @@ export class RecipeService {
 
     getRecipes(){
         return this.recipes.slice();
+    }
+
+    getRecipe(index: number){
+        return this.recipes[index];
     }
 
     addIngredientToSL(ingredients: Ingredient[]){
